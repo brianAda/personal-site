9db8db46dadbfd9d37e85f9306fe1b1ce46d622a
Brian Cooper <brian@adafruit.com>
2021-01-03 19:05:29 -0500

    Add js directory to eleventy config file

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 // passthrough https://michaelsoolee.com/add-css-11ty/
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('js');
   eleventyConfig.addPassthroughCopy('assets');
   return {
     passthroughFileCopy: true
